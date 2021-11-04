ed4ae9bf648019a8054503de5af1c5dfdbe282ae
wangteng <1638235292@qq.com>
2021-11-04 12:46:54 +0800

    Initial commit

---
+++ src/main/java/com/hitices/primeab/controller/MController.java
@@ -0,0 +1,33 @@
+package com.hitices.primeab.controller;
+
+import com.hitices.common.bean.MResponse;
+import com.hitices.mclient.annotation.MFuncProcess;
+import com.hitices.mclient.base.Action;
+import com.hitices.mclient.base.MObject;
+import com.hitices.mclient.core.MServiceSkeleton;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MController extends MObject {
+    @MFuncProcess(value = {"PrimeService.prime1"})
+    @PostMapping(value = "/primea")
+    public MResponse isPrimea(Action action, @RequestParam Integer n){
+//        if (primeService.prime1(n)){
+//            return primeService.prime2(n);
+//        }
+//        return false;
+        return MServiceSkeleton.getInstance().runProcess(action,n);
+    }
+
+    @MFuncProcess(value = {"PrimeService.prime2"})
+    @PostMapping(value = "/primeb")
+    public MResponse isPrimeb(Action action, @RequestParam Integer n){
+//        if (primeService.prime1(n)){
+//            return primeService.prime2(n);
+//        }
+//        return false;
+        return MServiceSkeleton.getInstance().runProcess(action,n);
+    }
+}
